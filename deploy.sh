5739e2ee80a0423e164fb7070bc05cf5a2fab1c7
Andrew Weida <anweid@mlc411.rmt.sas.com>
2020-07-01 18:32:53 -0400

    fixed a typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t identity404/multi-client:latest -t identity404/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t identity404/multi-server:latest -t identity404/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t identity404/multi-worker:latest -t identity404/multi-worker:$GIT_SHA -f ./woker/Dockerfile ./worker
+docker build -t identity404/multi-worker:latest -t identity404/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
 docker push identity404/multi-client:latest
 docker push identity404/multi-server:latest
@@ -11,7 +11,6 @@ docker push identity404/multi-server:$GIT_SHA
 docker push identity404/multi-worker:$GIT_SHA
 
 kubectl apply -f k8s
-
 
 kubectl set image deployments/server-deployment server=identity404/multi-server:$GIT_SHA 
 kubectl set image deployments/client-deployment client=identity404/multi-client:$GIT_SHA
